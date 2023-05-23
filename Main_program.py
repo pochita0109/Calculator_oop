a21488c5edf9969b9df8ee883093125c8bfe7ce1
pochita0109 <akosicosta@gmail.com>
2023-05-22 17:30:03 -0700

    Created files, pseudocodes

---
+++ Main_program.py
@@ -0,0 +1,12 @@
+# Kenneth John Costa
+# Assignment 7
+# Calculator in OOP
+
+# Ask the user to enter the first number
+# Ask the user to enter the second number
+# Ask the user about the operation they want to use 
+# If addition:
+# If subraction:
+# If multiplication:
+# If division:
+# Ask the user if they want to try again
