896a1104ed3f1f1a61c5623a37561169c7f5a075
pochita0109 <akosicosta@gmail.com>
2023-05-22 18:18:30 -0700

    Asked the user for first and second number

--- Main_program.py
+++ Main_program.py
@@ -2,8 +2,14 @@
 # Assignment 7
 # Calculator in OOP
 
+from User_interface import UserInterface
+
+ui = UserInterface()
+
 # Ask the user to enter the first number
+first_number = ui.input_first()
 # Ask the user to enter the second number
+second_number = ui.input_second()
 # Ask the user about the operation they want to use 
 # If addition:
 # If subraction:
--- User_interface.py
+++ User_interface.py
@@ -1,2 +1,17 @@
 # Create a class for user interface
 class UserInterface:
+    def input_first(self):
+        while True:
+            try:
+                first_number = float(input("Please enter the first number: "))
+                return first_number
+            except ValueError:
+                print("ERROR! THE CHARACTER YOU HAVE ENTERED IS INVALID!")
+    
+    def input_second(self):
+        while True:
+            try: 
+                second_number = float(input("Please enter the second number: "))
+                return second_number
+            except ValueError:
+                print("ERROR! THE CHARACTER YOU HAVE ENTERED IS INVALID!")
