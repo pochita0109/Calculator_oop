205dc0cf6540c0f286478702286b8eec73344922
pochita0109 <akosicosta@gmail.com>
2023-06-03 22:26:44 -0700

    Minor changes and added some designs

--- Updated_User_interface.py
+++ Updated_User_interface.py
@@ -2,17 +2,17 @@ from User_interface import UserInterface
 
 class Updated_User_interface(UserInterface):
     def name_input(self):
-        name = input("Please enter your name: ")
+        name = input("\033[95m\033[1mPlease enter your name: ")
         return name
     
     def print_sum(self, sum, name):
-        print(str(name) + ", " + "The sum is " + str(sum))
+        print("\033[92m\033[1mHey " + str(name) + ", " + "The sum is " + str(sum))
 
     def print_difference(self, difference, name):
-        print(str(name) + ", " + "The difference is " + str(difference))
+        print("\033[92m\033[1mHey " + str(name) + ", " + "The difference is " + str(difference))
     
     def print_product(self, product, name):
-        print(str(name) + ", " + "The product is " + str(product))
+        print("\033[92m\033[1mHey " + str(name) + ", " + "The product is " + str(product))
     
     def print_quotient(self, quotient, name):
-        print(str(name) + ", " + "The quotient is " + str(quotient))
+        print("\033[92m\033[1mHey " + str(name) + ", " + "The quotient is " + str(quotient))
