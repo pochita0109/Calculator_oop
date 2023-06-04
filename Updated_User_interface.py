5d776e5985db58bc329c997b7b40492c27548547
pochita0109 <akosicosta@gmail.com>
2023-06-03 20:36:29 -0700

    Inherited the user interface class and added name input

---
+++ Updated_User_interface.py
@@ -0,0 +1,6 @@
+from User_interface import UserInterface
+
+class Updated_User_interface(UserInterface):
+    def name_input(self):
+        name = input("Please enter your name: ")
+        return name
