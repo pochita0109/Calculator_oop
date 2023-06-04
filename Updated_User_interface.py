7d6b99bab50b5bb1aef3f2deb6778f7ff1bb864e
pochita0109 <akosicosta@gmail.com>
2023-06-03 22:09:07 -0700

    Override the print method

--- Updated_User_interface.py
+++ Updated_User_interface.py
@@ -4,3 +4,15 @@ class Updated_User_interface(UserInterface):
     def name_input(self):
         name = input("Please enter your name: ")
         return name
+    
+    def print_sum(self, sum, name):
+        print(str(name) + ", " + "The sum is " + str(sum))
+
+    def print_difference(self, difference, name):
+        print(str(name) + ", " + "The difference is " + str(difference))
+    
+    def print_product(self, product, name):
+        print(str(name) + ", " + "The product is " + str(product))
+    
+    def print_quotient(self, quotient, name):
+        print(str(name) + ", " + "The quotient is " + str(quotient))
